efb734927a59eec61881857e3bd3f2c820dfbd46
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-08 22:25:13 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,16 @@
+// Combining Operators
+let num = 2 + 4 * 2;
+num++
+console.log(num);
+
+let result = 12 - 3 / 3;
+result++
+console.log(result);
+
+let greek = 13 * 13 + 2 ;
+greek--
+console.log(greek);
+
+let sum = 7 + 7 - 1 * 0;
+sum++
+console.log(sum);
